4cd9ef84441399333fc15a55ce9ed32c25e5422f
Leonard Mateus <leogurning@gmail.com>
2026-07-30 18:52:21 +0700

    feat: implement users service and update users module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  getAllUsers() {
+    return this.usersRepository.getAllUsers();
+  }
+
+  async getUserById(id: number) {
+    const user = await this.usersRepository.getUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async createUser(data: CreateUserDto) {
+    const existingUser = await this.usersRepository.getUserByEmail(data.email);
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.usersRepository.createUser({
+      ...data,
+      password: hashedPassword,
+    });
+  }
+
+  async updateUser(id: number, data: UpdateUserDto) {
+    const user = await this.usersRepository.getUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (data.email && data.email !== user.email) {
+      const existingUser = await this.usersRepository.getUserByEmail(
+        data.email,
+      );
+      if (existingUser) {
+        throw new ConflictException('Email already exists');
+      }
+    }
+
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+
+    return this.usersRepository.updateUser(id, data);
+  }
+
+  async deleteUser(id: number) {
+    const user = await this.usersRepository.getUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.usersRepository.deleteUser(id);
+  }
+}
